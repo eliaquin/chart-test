37f40574e0d831ee1534da3e8ff37103331782a7
Eliaquín Encarnación <eliaquin@gmail.com>
2020-08-29 09:54:02 -0400

    Created bar graphics

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import StackedChart from "./components/StackedChart";
 
 function App() {
+  const barValues = [{ s1: 20, s2: 30 }];
+  // Generate rest of the data
+  for (let index = 25; index < 90; index += 5) {
+    const x = index;
+    const y = 100 - index;
+    barValues.push({ s1: x, s2: y });
+  }
+  barValues.push({ s1: 30, s2: 5 });
+  const lineValues = [
+    { x: 1, y: 90 },
+    { x: 2, y: 87 },
+    { x: 3, y: 80 },
+    { x: 4, y: 74 },
+    { x: 5, y: 70 },
+    { x: 6, y: 64 },
+    { x: 7, y: 59 },
+    { x: 8, y: 53 },
+    { x: 9, y: 45 },
+    { x: 10, y: 40 },
+    { x: 11, y: 38 },
+    { x: 12, y: 25 },
+    { x: 13, y: 18 },
+    { x: 14, y: 10 },
+    { x: 15, y: 0 },
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <StackedChart barValues={barValues} lineValues={lineValues} />
     </div>
   );
 }
---
+++ src/components/StackedChart/StackedChart.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { VictoryChart, VictoryStack, VictoryBar, VictoryLine } from "victory";
+
+export default function StackedChart(props) {
+  const { barValues, lineValues } = props;
+  const bar1 = barValues.map((el, idx) => ({ x: idx + 1, y: el.s1 }));
+  const bar2 = barValues.map((el, idx) => ({ x: idx + 1, y: el.s2 }));
+
+  console.log("bar1", bar1);
+  return (
+    <div>
+      <VictoryChart domain={{ x: [0, barValues.length], y: [0, 100] }}>
+        <VictoryStack colorScale={["gray", "lightgray"]}>
+          <VictoryBar data={bar1} />
+          <VictoryBar data={bar2} />
+        </VictoryStack>
+        <VictoryLine
+          style={{
+            data: { stroke: "#2196f3" },
+            parent: { border: "1px solid #ccc" },
+          }}
+          interpolation="natural"
+          data={lineValues}
+        />
+      </VictoryChart>
+    </div>
+  );
+}
